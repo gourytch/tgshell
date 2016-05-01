45e5aba534532b3549626cb165ae10e2a2647b8d
Gourytch <gour@mail.ru>
2016-05-01 22:05:36 +0500

    SCROT: photo -> document

--- handle_screenshot.go
+++ handle_screenshot.go
@@ -12,8 +12,10 @@ func handle_scrot(m *tgbotapi.Message) {
 	if dsp == "" {
 		dsp = config.Display
 	} else {
-		config.Display = dsp
-		SaveConfig()
+		if config.Display != dsp {
+			config.Display = dsp
+			SaveConfig()
+		}
 	}
 	fname := "scrot_" + time.Now().Format("20060102_150405") + ".jpg"
 	out, err := shell.SetEnv("DISPLAY", dsp).
@@ -23,7 +25,8 @@ func handle_scrot(m *tgbotapi.Message) {
 	if err != nil {
 		send_reply(m, fmt.Sprintf("scrot error: %s\nmessage:\n%s", err, out))
 	} else {
-		send_reply_image(m, config.Data_Dir+"/"+fname, nil)
+		//send_reply_image(m, config.Data_Dir+"/"+fname, nil)
+		send_reply_document(m, config.Data_Dir+"/"+fname, nil)
 	}
 
 }
