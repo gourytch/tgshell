4b9e4a73908b7180a598407b0e3e2f4c4549c1bf
Lev V. Babchenko <gour@mail.ru>
2017-06-20 22:20:14 +0500

    recover scrot ability (linux part #5)

--- scrot_linux.go
+++ scrot_linux.go
@@ -9,7 +9,7 @@ import (
 func scrot(lossless bool) (data []byte, err error) {
 	full_name := RealPath(config.Data_Dir + scrotFName(lossless))
 	CheckDatadir()
-	_, err := shell.SetEnv("DISPLAY", config.Display).
+	_, err = shell.SetEnv("DISPLAY", config.Display).
 		SetDir(config.Data_Dir).
 		Command("scrot", full_name).
 		CombinedOutput()
